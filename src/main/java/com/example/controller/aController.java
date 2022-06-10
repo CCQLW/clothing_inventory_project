d809200b06022d88e17ca66855739d3df4f4c1f1
Q <1340460901@qq.com>
2022-06-10 14:44:25 +0800

    完成记住账户和密码按钮

--- src/main/java/com/example/controller/aController.java
+++ src/main/java/com/example/controller/aController.java
@@ -4,13 +4,19 @@ package com.example.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Arrays;
 
 @Controller
+
 public class aController {
-
-    @RequestMapping("/a")
-    public String a() {
-        return "login.html";
+    @ResponseBody
+    @GetMapping("/a")
+    public String a(HttpServletRequest request) {
+//        String aaaaa = (String) request.getSession().getAttribute("aaaaa");
+//        System.out.println("aaaaa:" + aaaaa);
+        return "<html><body><h1>A</h1><div id='created'>CST 2022</div><div>asd</div></body></html>";
     }
-
 }
